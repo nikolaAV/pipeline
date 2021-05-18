792640c9c3a7724881756bd8021e2b5c17dc54d0
Alexander Nikolaenko <Alexander.Nikolaenko@apostera.com>
2021-05-18 13:58:22 +0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+    g++ main.cpp -std=c++14 -o exe
+*/
+
+#include "test_transform_iterator.cpp"
+
+int main()
+{
+   test_transform_iterator::algo_transform();
+   test_transform_iterator::algo_transform_if();
+}
